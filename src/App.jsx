8100e64bba14acf4e3f8f56ec258d2c620503af6
Gappy <lucasamsg6@gmail.com>
2026-06-11 10:59:39 -0300

    Fix: Pagina de catalogo ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import HomePage from './pages/HomePage'
 import CatalogPage from './pages/CatalogPage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/catalogo" element={<CatalogPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalogo" element={<CatalogPage />} />
+      </Routes>
+    </>
   )
 }
